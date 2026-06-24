18be0e09d188b99e345d17edfb158b8e9008a535
Azamathub <oybekmuzafarov46@email.com>
2026-06-24 18:21:01 +0500

    photo fix

--- bot.py
+++ bot.py
@@ -17,6 +17,8 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+MANZIL_PHOTO_ID = "AgACAгIAAxkBAAIBKmo7xVOnCilgZSnOMJFJDwMn7U9JAAKjGWsbirjYSZhZUiZAcqfGAQADAgADeQADPAQ"
+
 def get_main_keyboard():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
@@ -49,7 +51,7 @@ async def process_location(message: types.Message):
     await message.react(reaction=[ReactionTypeEmoji(emoji="🔥")])
     await bot.send_photo(
         chat_id=message.chat.id,
-        photo="photo="AgACAгIAAxkBAAIBKmo7xVOnCilgZSnOMJFJDwMn7U9JAAKjGWsbirjYSZhZUiZAcqfGAQADAgADeQADPAQ",",
+        photo=photo="AgACAгIAAxkBAAIBKmo7xVOnCilgZSnOMJFJDwMn7U9JAAKjGWsbirjYSZhZUiZAcqfGAQADAgADeQADPAQ",,
         caption="📍 Bekobod shahar Tohir va Zuhra savdo kompleksi 1-qavatida 112-dokon."
     )
     await message.reply_location(latitude=40.2140770, longitude=69.2654280)
